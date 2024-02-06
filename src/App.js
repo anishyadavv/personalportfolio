30876e1ab02e1f3089ba794cd63d30b4a12f360e
anish <anishyadav7853@gmail.com>
2024-02-06 21:55:52 +0530

    added lazy loading for projects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import "./App.css";
+import React, { Suspense, lazy } from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import useWindowSize from "./hooks/useWindowSize";
+import Skeleton from "react-loading-skeleton";
 
-import './App.css';
-import About from './components/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import Projects from './components/Projects';
-import Sidebar from './components/Sidebar';
-import useWindowSize from './hooks/useWindowSize';
+const Projects = lazy(() => import("./components/Projects"));
+
 function App() {
   const size = useWindowSize();
   return (
@@ -15,7 +18,9 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      <Projects />
+      <Suspense fallback={<Skeleton count={5} />}>
+        <Projects />
+      </Suspense>
       <Contact />
       <Footer />
       {size.width > 600 && <Sidebar />}
